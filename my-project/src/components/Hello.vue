66d4051eee8cc8a8f3e321c95a30189fdc8acba1
lxw666 <1173442657@qq.com>
2017-06-02 17:01:25 +0800

    edit

--- my-project/src/components/Hello.vue
+++ my-project/src/components/Hello.vue
@@ -75,10 +75,10 @@
     ul {
         list-style-type: none;
         padding: 0;
-        li {
-            display: inline-block;
-            margin: 0 10px;
-        }
+    }
+    li {
+        display: inline-block;
+        margin: 0 10px;
     }
     a {
         color: #42b983;
